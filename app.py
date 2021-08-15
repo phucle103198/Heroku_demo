4150791d888d08a16a41b1bbddeca4921895085e
phucle103198 <52406323+phucle103198@users.noreply.github.com>
2021-08-15 16:19:41 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
-
+import pandas as pd
+import csv
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl','rb'))
@@ -17,7 +18,7 @@ def home():
 def predict():
     '''
     For rendering results on HTML GUI
-    '''
+
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
@@ -25,6 +26,16 @@ def predict():
     output = round(prediction[0], 2)
 
     return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+    '''
+
+    f = request.form['file']
+    data = []
+    with open(f) as file:
+        csvfile = csv.reader(file)
+        for row in csvfile:
+            data.append(row)
+    return render_template('data.html', data=data)
+
 
 
 if __name__ == "__main__":
